a80e0b9779cf0f3afda381ecc3e637d8551460db
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-09-19 20:07:39 +0530

    Day 2
    Day 2 final commit

--- Hackerearth/no_of_steps.cpp
+++ Hackerearth/no_of_steps.cpp
@@ -1,50 +1,75 @@
-
+// C++ implementation to find the
+// minimum number of steps to convert
+// array by subtracting the corresponding
+// element from array B
 #include <bits/stdc++.h>
-
 using namespace std;
-int areSame(int arr[],
-             int n)
+
+// Function to find the minimum steps
+void minimumSteps(int ar1[], int ar2[], int n)
 {
-  int first = arr[0];
- 
-  for (int i = 1; i < n; i++)
-    if (arr[i] != first)
-      return 0;
-  return 1;
+	
+	// Counter to store the steps
+	int ans = 0;
+	
+	// Flag to check that
+	// array is converted
+	bool flag = true;
+	
+	// Loop until the minimum of the
+	// array is greater than -1
+	while (*min_element(ar1, ar1 + n) > -1)
+	{
+		int a = *min_element(ar1, ar1 + n);
+		
+		// Loop to convert the array
+		// elements by substraction
+		for(int i = 0; i < n; i++)
+		{
+			if (ar1[i] != a)
+			{
+				ar1[i] -= ar2[i];
+				ans += 1;
+			}
+		}
+		
+		set<int> s1(ar1, ar1 + n);
+		
+		// If the array is converted
+		if (s1.size() == 1)
+		{
+			flag = false;
+			cout << ans << endl;
+			break;
+		}
+	}
+	
+	if (flag)
+	{
+		cout << -1 << endl;
+	}
 }
-int main() {
-	int n,i,j;
-	bool same;
-	cin >> n;										
-	int a[n],b[n];	
-    for(i=0;i<n;i++){
-        cin>>a[i];
+		
+// Driver Code
+int main()
+{
+	int n,i;
+	cin>>n;
+	
+	int ar1[n];
+	int ar2[n];
+	for(i=0;i<n;i++){
+        cin>>ar1[i];
     }
     
      for(i=0;i<n;i++){
-        cin>>b[i];
+        cin>>ar2[i];
     }
-    j=0;
-    for(i=0;i<n;i++){
-            j++;
-            
-       if(a[i]>=b[i]){
-           a[i]=a[i]-b[i];
-       }
-        int k=areSame(a,n);
-       if(k==1){
-        cout<<j;
+	
+	// Function call
+	minimumSteps(ar1, ar2, n);
 
-       }
-       else{
-           if(i==n-1)
-            cout<<"-1";
-           }
-       
-    }
-    
-return 0;
+	return 0;
 }
 
-
-
+// This code is contributed by rutvik_56
