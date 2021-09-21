cd1f40862e6050b3628cdb828db1a533681a46a2
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-09-21 22:12:19 +0530

    Day4 Commits
    Cmmit done for day4.

---
+++ Hackerearth/maximum_border.cpp
@@ -0,0 +1,28 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int test;
+	cin >> test;	
+    int n,m;
+    cin>>n>>m;
+     int a[n][m];
+     for(int b=0;b<test;b++){
+      for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++)
+         {
+                   cin>>a[i][j];
+
+         }
+     }
+     }
+   
+	
+    cout<<"No";
+
+    return 0;							
+}
+
