e3e3d3d0f138bb27fb3bbb686a3a4648e22eaded
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-09-28 19:57:36 +0530

    Update Split_houses.cpp
    Split house code one only optimization left.

--- Hackerearth/Split_houses.cpp
+++ Hackerearth/Split_houses.cpp
@@ -1,23 +1,44 @@
 
 
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    string word;
-   	cin >>word;
-   	char z='z';
-   	char o ='o';
-   	int x;
-   	x=count(word.begin(), word.end(), 'z');
-   	int y;
-   	y=count(word.begin(), word.end(), 'o');
-   
-   	if(y==2*x)
-   	 cout<<"Yes";
+	int n,temp=0;
+	cin>>n;
+    char word[n];
+   	for(int i=0;i<n;i++)
+	   {
+		   cin>>word[i];
+		   
+	    }
+
+			for(int i=0;i<n;i++)
+	   {
+		   if(word[i]=='H'&&word[i+1]=='H'){
+              temp++;
+			  break;
+		   }
+
+			if(word[i]=='.'){
+				word[i]='B';
+
+			}
+	    }
+
+
+   	
+   	if(temp==0){
+
+        	 cout<<"YES\n";
+			 for(int i=0;i<n;i++){
+				 cout<<word[i];
+			 }
+
+	   }
    	else
-   	 cout<<"No";
+   	 cout<<"NO";
 	
 	
 
