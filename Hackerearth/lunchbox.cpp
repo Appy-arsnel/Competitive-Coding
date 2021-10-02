114fcb1b381bd7acb322e7d15cc2308d0a76f45e
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2021-10-02 17:31:11 +0530

    Create lunchbox.cpp

---
+++ Hackerearth/lunchbox.cpp
@@ -0,0 +1,23 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int t,n,m,;
+	cin >> t;
+    while(t>0){
+       cin>>n>>m;
+       int a[m];
+       for(int i=0;i<m;i++){
+           cin>> a[i];
+       }
+
+      t--;  
+    }	
+    
+
+    return 0;							
+}
+
